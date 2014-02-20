8fc3148aa6b3fb911fea58ce60ac22347e62944d
Diane Trout <diane@ghic.org>
2014-02-19 22:35:03 -0800

    Imported Upstream version 0.7.80

--- contact/src/contact-wrapper.h
+++ contact/src/contact-wrapper.h
@@ -23,22 +23,26 @@
 #include <QtCore/QObject>
 
 #include <TelepathyQt/Account>
-#include <TelepathyQt/Contact>
+#include <KTp/contact.h>
 
 class ContactWrapper : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(bool isAccountOnline READ isAccountOnline NOTIFY accountOnlineChanged)
+    Q_PROPERTY(bool isContactOnline READ isContactOnline NOTIFY contactOnlineChanged)
+    Q_PROPERTY(QString avatar READ avatar NOTIFY avatarChanged)
+
+    Q_PROPERTY(bool canSendFile READ canSendFile NOTIFY canSendFileChanged)
+    Q_PROPERTY(bool canStartTextChat READ canStartTextChat NOTIFY canStartTextChatChanged)
+    Q_PROPERTY(bool canStartAudioCall READ canStartAudioCall NOTIFY canStartAudioCallChanged)
+    Q_PROPERTY(bool canStartVideoCall READ canStartVideoCall NOTIFY canStartVideoCallChanged)
+
+    Q_PROPERTY(QString displayName READ displayName NOTIFY displayNameChanged)
+    Q_PROPERTY(QString presenceStatus READ presenceStatus NOTIFY presenceStatusChanged)
+
 public:
     ContactWrapper(QObject *parent = 0);
     virtual ~ContactWrapper();
-
-    Q_PROPERTY(bool accountOnline READ isAccountOnline);
-    Q_PROPERTY(QString avatar READ avatar);
-    Q_PROPERTY(bool canSendFile READ canSendFile);
-    Q_PROPERTY(bool canStartAudioCall READ canStartAudioCall);
-    Q_PROPERTY(bool canStartVideo READ canStartVideo);
-    Q_PROPERTY(QString displayName READ displayName);
-    Q_PROPERTY(QString presenceStatus READ presenceStatus);
 
     /** returns the account id related to the contact chosen */
     QString accountId() const;
@@ -49,20 +53,26 @@ public:
     /** returns whether the contact can send files */
     bool canSendFile() const;
 
+    /** returns whether the contact support textual chat */
+    bool canStartTextChat() const;
+
     /** returns whether the contact can start/receive audio calls */
     bool canStartAudioCall() const;
 
     /** returns whether the contact can start/receive video */
-    bool canStartVideo() const;
+    bool canStartVideoCall() const;
 
     /** returns current contact being represented */
-    Tp::ContactPtr contact() const;
+    KTp::ContactPtr contact() const;
 
     /** returns the display name of the contact */
     QString displayName() const;
 
     /** returns whether the account is online/offline */
     bool isAccountOnline() const;
+
+    /** returns whether the contact is online/offline */
+    bool isContactOnline() const;
 
     /** returns the contact presence status (online, offlince ... ) */
     QString presenceStatus() const;
@@ -87,30 +97,41 @@ public:
 
 public slots:
     void sendMail();
-    void startAudioCall();
-    void startFileTransfer();
     void startTextChat();
+    void startAudioCall();
     void startVideoCall();
+    void startFileTransfer();
+    void startFileTransfer(const QVariantList &urls);
 
 signals:
-    void accountPresenceChanged();
+    void accountOnlineChanged();
     void avatarChanged();
-    void newContactSet();
-    void presenceChanged();
+
+    void canSendFileChanged();
+    void canStartTextChatChanged();
+    void canStartAudioCallChanged();
+    void canStartVideoCallChanged();
+
+    void displayNameChanged();
+    void presenceStatusChanged();
+    void contactOnlineChanged();
+
+    void contactChanged();
 
 private slots:
     void genericOperationFinished(Tp::PendingOperation *op);
     void onConnectionChanged(const Tp::ConnectionPtr &newConn);
     void onContactManagerStateChanged(Tp::ContactListState state);
+    void updateProperties();
 
 private:
-    void setupAccountConnects();
-    void setupContactConnects();
-    void undoAccountConnects();
-    void undoContactConnects();
+    void connectAccountSignals();
+    void connectContactSignals();
+    void disconnectAccountSignals();
+    void disconnectContactSignals();
 
     Tp::AccountPtr m_account;
-    Tp::ContactPtr m_contact;
+    KTp::ContactPtr m_contact;
     QString m_tempAvatar;           /** this is the path to the cached avatar for when kde-telepathy is offline */
     QString m_tempContactId;
 };
