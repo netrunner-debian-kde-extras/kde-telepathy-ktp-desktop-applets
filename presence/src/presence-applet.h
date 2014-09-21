78bc7189162ccb656aa84548d32ba961215510ea
Diane Trout <diane@ghic.org>
2014-09-20 20:29:30 -0700

    Imported Upstream version 0.8.80

--- presence/src/presence-applet.h
+++ presence/src/presence-applet.h
@@ -32,6 +32,7 @@
 
 namespace KTp {
     class GlobalPresence;
+    class PresenceModel;
 }
 
 namespace Tp {
@@ -68,7 +69,7 @@ private Q_SLOTS:
     void toolTipAboutToShow();
     void toolTipHidden();
 
-    void startAccountManager();
+    void showSettingsKCM();
     void toggleContactList();
     void onAddContactRequest();
     void onMakeCallRequest();
@@ -98,8 +99,7 @@ private:
     QDBusServiceWatcher  *m_contactListWatcher;
     bool                  m_contactListRunning;
 
-    void addPresence(const KTp::Presence &presence);
-    QList<KTp::Presence> m_presences;
+    KTp::PresenceModel *m_presences;
 };
 
 #endif  // KTP_PRESENCE_APPLET_H
